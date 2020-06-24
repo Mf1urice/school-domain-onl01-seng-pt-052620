8fa72cd0d78ff3eb694700ed5595dc532571cc72
Maurice Falls <falls8019oct@gmail.com>
2020-06-24 01:35:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
+=======
+# code here!
+>>>>>>> e760ca11813d3c2d3b8fcb6c2ecf3a342da13fbf
 class School
   attr_accessor :name, :roster
   
@@ -7,21 +11,38 @@ class School
   end 
      def add_student(name, grade)
        if roster.has_key?(grade)
+<<<<<<< HEAD
        roster[grade] << name 
      else roster[grade] = [name]
+=======
+       roster(grade) << (name) 
+     else roster(grade) = name
+>>>>>>> e760ca11813d3c2d3b8fcb6c2ecf3a342da13fbf
      end 
 end
 
     def grade(grade)
+<<<<<<< HEAD
       @roster[grade]
+=======
+      @roster(grade)
+>>>>>>> e760ca11813d3c2d3b8fcb6c2ecf3a342da13fbf
     end
     
     def  sort 
       sorted={}
       roster.each do |grade, name|
+<<<<<<< HEAD
         sorted[grade]=name.sort 
+=======
+        sorted(grade)=name.sort 
+>>>>>>> e760ca11813d3c2d3b8fcb6c2ecf3a342da13fbf
       end 
       
       sorted
     end
 end
+<<<<<<< HEAD
+=======
+
+>>>>>>> e760ca11813d3c2d3b8fcb6c2ecf3a342da13fbf
